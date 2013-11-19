37f0b3d27d5d18428779750835fe857f61a22058
Andrew Friedrich <afriedrich@glgroup.com>
2013-11-19 13:18:51 -0600

    init checkin

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var tools = require('./routes/tools.js');
+
+app.configure(function() {
+  app.set('startedOn', new Date());
+  app.set('root', __dirname);
+  app.set('port', process.argv[2] || 9090);
+  app.use(express.logger(process.env.NODE_ENV == 'production' ? 'default' : 'dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('cookies!'));
+  //app.use(express.static(path.join(app.get('root'), 'public')));
+  app.use(app.router);
+});
+
+app.use(express.logger());
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/diag*', tools.diagnostic(app));
+
+var server = http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
+  console.log('listening on port ' + app.get('port'));
+});
+
+server.on('close', function() {
+  console.log('server closed on port ' + app.get('port'));
+});
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGABRT', 'SIGTERM'].forEach(function(s) {
+  process.on(s, function() {
+    server.close(); 
+  });
+});
